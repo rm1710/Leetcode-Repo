d49ffbaec9394d0ef9480f9b1670299b3671cd0d
Ritesh Sunil Mishra <162662643+rm1710@users.noreply.github.com>
2024-08-06 18:48:02 +0530

    Time: 9 ms (61.15%), Space: 45.7 MB (31.73%) - LeetHub

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumPushes(String word) {
+        //frequency
+        int arr[]=new int[26];
+        for(char ch: word.toCharArray()){
+            arr[ch-'a']++;
+        }
+        //dec order sort / pq
+        Arrays.sort(arr);
+        int count=0;
+        int minKeyPress=0;
+        //rev
+        for(int i=25;i>=0;i--){ 
+            if(arr[i]==0){break;}
+            int val=arr[i]*(count/8+1);
+            minKeyPress +=val;
+            count++;
+        }
+        return minKeyPress;
+    }
+}
