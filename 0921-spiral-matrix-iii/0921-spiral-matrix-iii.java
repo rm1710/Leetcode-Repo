c204350e81754bb259206a1f9575f9575e14694b
Ritesh Sunil Mishra <162662643+rm1710@users.noreply.github.com>
2024-08-08 17:11:52 +0530

    Time: 4 ms (81.31%), Space: 45.1 MB (57.24%) - LeetHub

--- 0921-spiral-matrix-iii/0921-spiral-matrix-iii.java
+++ 0921-spiral-matrix-iii/0921-spiral-matrix-iii.java
@@ -1,34 +1,23 @@
 class Solution {
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int directions[][] = {
-            {0, 1},   // Move right
-            {1, 0},   // Move down
-            {0, -1},  // Move left
-            {-1, 0}   // Move up
-        };
+        int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        int[][] res = new int[rows * cols][2];
+        int count = 0, step = 1, index = 0;
 
-        int n = rows * cols;
-        int res[][] = new int[n][2];
-        res[0][0] = rStart;
-        res[0][1] = cStart;
-        int count = 1;
-        int step = 1;
-        int index = 0;
+        res[count++] = new int[]{rStart, cStart};
 
-        while (count < n) {
+        while (count < rows * cols) {
             for (int times = 0; times < 2; times++) {
-                int dr = directions[index % 4][0];
-                int dc = directions[index % 4][1];
+                int dr = directions[index][0];
+                int dc = directions[index][1];
                 for (int i = 0; i < step; i++) {
                     rStart += dr;
                     cStart += dc;
                     if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols) {
-                        res[count][0] = rStart;
-                        res[count][1] = cStart;
-                        count++;
+                        res[count++] = new int[]{rStart, cStart};
                     }
                 }
-                index++;
+                index = (index + 1) % 4;
             }
             step++;
         }
