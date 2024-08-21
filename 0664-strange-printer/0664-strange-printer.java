fd0f90d9c0f1991973c3a3a3d0c3fef5dc1315d4
Ritesh Sunil Mishra <162662643+rm1710@users.noreply.github.com>
2024-08-21 18:10:48 +0530

    Time: 13 ms (74.29%), Space: 43.3 MB (44.49%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.java
+++ 0664-strange-printer/0664-strange-printer.java
@@ -3,18 +3,17 @@ class Solution {
         int n = s.length();
         if (n == 0) return 0;
 
-        // dp[i][j] will be the minimum number of turns to print s[i..j]
         int[][] dp = new int[n][n];
 
         for (int i = n - 1; i >= 0; i--) {
-            dp[i][i] = 1; // A single character requires 1 turn to print
+            dp[i][i] = 1;
             for (int j = i + 1; j < n; j++) {
-                // Initialize dp[i][j] with printing s[j] after s[i..j-1]
                 dp[i][j] = dp[i][j - 1] + 1;
-                // Try to find a better solution by merging prints
-                for (int k = i; k < j; k++) {
-                    if (s.charAt(k) == s.charAt(j)) {
-                        dp[i][j] = Math.min(dp[i][j], dp[i][k] + (k + 1 <= j - 1 ? dp[k + 1][j - 1] : 0));
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i][j - 1];
+                } else {
+                    for (int k = i; k < j; k++) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k + 1][j]);
                     }
                 }
             }
