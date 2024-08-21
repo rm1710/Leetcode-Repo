44e61876e7b073009d92db511d4372c550b2dd62
Ritesh Sunil Mishra <162662643+rm1710@users.noreply.github.com>
2024-08-21 18:08:51 +0530

    Time: 7 ms (100%), Space: 43.1 MB (69.39%) - LeetHub

---
+++ 0664-strange-printer/0664-strange-printer.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int strangePrinter(String s) {
+        char[] sc=s.toCharArray();
+        final int n=removeDuplicates(sc);
+        if(n<=1) return n;
+        return dfs(0,n-1,sc,new int[n][n]);
+    }
+
+    private int dfs(int left,int right,char[] sc,int[][] memo){
+        if(left>=right){
+            return (left==right)?1:0;
+        }
+        if(memo[left][right]!=0) return memo[left][right];
+        int letter = sc[left];
+        int answer = 1 + dfs(left+1,right,sc,memo);
+        for(int k=left+1;k<=right;k++){
+            if(sc[k] == letter){
+                answer=Math.min(answer,dfs(left+1,k-1,sc,memo)+dfs(k,right,sc,memo));
+            }
+        }
+        return memo[left][right]=answer;
+    }
+
+    private int removeDuplicates(char[] sc){
+        int idx=0;
+        char prev=0;
+
+        for(char c:sc){
+            if(c!=prev){
+                sc[idx++]=c;
+                prev=c;
+            }
+        }
+        return idx;
+    }
+}
