37a022ada53b601b0e1d09823f0f6e009e3e48b6
Ritesh Sunil Mishra <162662643+rm1710@users.noreply.github.com>
2024-08-22 18:44:10 +0530

    Time: 0 ms (100%), Space: 40.3 MB (67.03%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,7 +1,6 @@
 class Solution {
     public int findComplement(int num) {
-        int len = (int)(Math.log(num) / Math.log(2))+1;
-        int mask=(1<<len)-1;
-        return mask ^ num;
+        int mask = (Integer.highestOneBit(num) << 1) - 1;
+        return num ^ mask;
     }
 }
