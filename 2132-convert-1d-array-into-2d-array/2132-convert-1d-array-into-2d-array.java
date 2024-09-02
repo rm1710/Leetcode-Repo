92feed3e4ccdc91a91903cc19fea24e0067496b0
Ritesh Sunil Mishra <162662643+rm1710@users.noreply.github.com>
2024-09-02 11:49:38 +0530

    Time: 4 ms (70.89%), Space: 55.4 MB (57.52%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
@@ -2,9 +2,13 @@ class Solution {
     public int[][] construct2DArray(int[] original, int m, int n) {
         if (original.length != m * n) return new int[0][0]; // Check if reshape is possible
         
+        int index=0;
         int[][] arr = new int[m][n];
-        for (int i = 0; i < original.length; i++) {
-            arr[i / n][i % n] = original[i]; // Calculate row and column positions
+        for (int i = 0; i < m; i++) {
+            for(int j=0;j<n;j++){
+                arr[i][j]=original[index];
+                index++;
+            }
         }
         
         return arr;
