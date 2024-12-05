ad217d20dec5a9441604373b1b2b2d692482ffb5
Ritesh Sunil Mishra <162662643+rm1710@users.noreply.github.com>
2024-12-05 20:38:37 +0530

    Time: 2 ms (22.95%), Space: 41.3 MB (69.06%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,15 @@
+import java.util.regex.*;
+class Solution {
+    public int strStr(String haystack, String needle) {
+        Pattern pattern = Pattern.compile(needle);
+
+        Matcher match= pattern.matcher(haystack);
+
+        if(match.find()) {
+            return match.start();
+        }
+        else{
+            return -1;
+        }
+    }
+}
