20ca41aed82e4ed5d5b079d647b5ab5cbb016368
Ritesh Sunil Mishra <162662643+rm1710@users.noreply.github.com>
2025-06-06 21:26:11 +0530

    Time: 29 ms (90.34%), Space: 45.7 MB (63.39%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        HashSet<Integer> hs=new HashSet<>();
+
+        for(int c:candyType){
+            hs.add(c);
+        }
+
+        return Math.min(hs.size(),candyType.length/2);
+    }
+}
