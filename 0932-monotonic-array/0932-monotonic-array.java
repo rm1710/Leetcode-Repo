6323ac2d26a2966e6712f9d384b66ee6a9b327a0
Ritesh Sunil Mishra <162662643+rm1710@users.noreply.github.com>
2025-06-23 20:45:05 +0530

    Time: 3 ms (49.59%), Space: 58.9 MB (92.76%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean t=true, s=true;
+        
+
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<nums[i-1]){
+                t=false;
+            }
+            if(nums[i]>nums[i-1]){
+                s=false;
+            }
+        }
+
+        return t || s;
+    }
+}
