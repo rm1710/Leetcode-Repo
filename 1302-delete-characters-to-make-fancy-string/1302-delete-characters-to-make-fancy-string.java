19c95409657ae0d26b012201955c5f25fbb9ac39
Ritesh Sunil Mishra <162662643+rm1710@users.noreply.github.com>
2025-07-21 21:52:02 +0530

    Time: 34 ms (68.94%), Space: 45.7 MB (78.54%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -1,17 +1,16 @@
 class Solution {
     public String makeFancyString(String s) {
         StringBuilder sb = new StringBuilder();
-        int count = 1; // To count consecutive occurrences
+        int count = 1; 
 
         for (int i = 0; i < s.length(); i++) {
-            // Check if current character is same as the previous one
+            
             if (i > 0 && s.charAt(i) == s.charAt(i - 1)) {
                 count++;
             } else {
-                count = 1; // Reset count for a new character
+                count = 1; 
             }
 
-            // Append the character only if it appears less than or equal to 2 times consecutively
             if (count <= 2) {
                 sb.append(s.charAt(i));
             }
