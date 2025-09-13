9337569c898fe675f0d3868c8987ae299196c53b
Ritesh Sunil Mishra <162662643+rm1710@users.noreply.github.com>
2025-09-13 18:15:27 +0530

    Time: 1 ms (100%), Space: 42.3 MB (91.95%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -6,12 +6,11 @@ class Solution {
         }
 
         int maxVowel = 0, maxConsonant = 0;
-        String vowels = "aeiou";
-
+        
         for (int i = 0; i < 26; i++) {
             if (freq[i] > 0) {
                 char c = (char) (i + 'a');
-                if (vowels.indexOf(c) != -1)
+                if ("aeiou".indexOf(c) != -1)
                     maxVowel = Math.max(maxVowel, freq[i]);
                 else
                     maxConsonant = Math.max(maxConsonant, freq[i]);
