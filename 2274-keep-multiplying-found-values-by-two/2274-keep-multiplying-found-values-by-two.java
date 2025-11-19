0df40d31fc8e6d0f968254047c48665c8dac764e
Ritesh Sunil Mishra <162662643+rm1710@users.noreply.github.com>
2025-11-19 10:15:45 +0530

    Time: 1 ms (93.95%), Space: 45.3 MB (25.71%) - LeetHub

---
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        boolean found = true;
+
+        while (found) {
+            found = false;
+            for (int i : nums) {
+                if (i == original) {
+                    original *= 2;
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        return original;
+    }
+}
