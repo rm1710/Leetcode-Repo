1b631400b6f9fc04a2e750dc8ac6031d8d39f916
Ritesh Sunil Mishra <162662643+rm1710@users.noreply.github.com>
2025-11-29 16:05:00 +0530

    Time: 1 ms (99.8%), Space: 85.7 MB (9.27%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        boolean[] seen = new boolean[nums.length];
+        for (int num : nums) {
+            if (seen[num]) return num;
+            seen[num] = true;
+        }
+        return -1;
+    }
+}
